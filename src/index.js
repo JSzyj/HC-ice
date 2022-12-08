52ef4ef60b689479b687e8df95330edcf8e83919
yingjie <18855051882@163.com>
2022-12-09 00:03:47 +0800

    feat:HC-ice

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
+import './index.css'
+
 function init() {
-    console.log("HC-脚手架")
+    var element = document.createElement('div');
+
+    // lodash 是由当前 script 脚本 import 导入进来的
+    element.innerHTML = 'HC-ice1111';
++   element.classList.add('hello');
+
+    return element;
 }
-init()
+document.body.appendChild(init());
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,42 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
+
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
-      }
-  };
+    },
+
+    devServer: {
+        static: './dist'
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'HC-ice'
+        })
+    ],
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.less$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'less-loader',
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                ]
+            }
+        ]
+    }
+};
