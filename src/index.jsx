367b513e1fa913ff9ce474364012bedeba45dc30
yingjie <18855051882@163.com>
2022-12-12 13:42:36 +0800

    feat:''

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from 'react-dom/client';
 import { Button } from '@alifd/next';
+import Home from "./page/home/index.jsx";
 import'./index.css'
 
 const App = () => {
     return (
-        <Button type="primary" className="hello">Hello, HC-ICE</Button>
+        <>
+         <Button type="primary" className="hello">Hello, HC-ICE</Button>
+         <Home/>
+        </>
     )
 }
 const root = ReactDOM.createRoot(document.getElementById('root'));
